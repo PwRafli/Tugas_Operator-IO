7794d428b26c7a25d063f831e300f347b6df2da1
PwRafli <rafliefendymm@gmail.com>
2024-10-21 11:52:17 +0700

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,12 @@
+import math
+
+akar = math.sqrt(26)
+pangkat = math.pow(2,3)
+bulat_bawah = math.floor(2.4)
+bulat_atas = math.ceil(2.4)
+print()
+print(f"akar 26     = {akar}")
+print(f"2 pangkat 3 = {pangkat}")
+print(f"pembulatan ke bawah dari 2.4 = {bulat_bawah}")
+print(f"pembulatan ke atas dari 2.4  = {bulat_atas}")
+print()
