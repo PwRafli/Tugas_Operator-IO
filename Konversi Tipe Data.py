52c4fce6f334f9911fa3c9011d4f631a73e611e9
PwRafli <rafliefendymm@gmail.com>
2024-10-21 12:33:37 +0700

    Add files via upload

---
+++ Konversi Tipe Data.py
@@ -0,0 +1,11 @@
+print()
+bilangan = input('masukkan sebuah bilangan :')#input
+print("bilangan yang dimasukkan adalah : ",bilangan)#output
+#konversi
+imt_bilangan = int(bilangan)
+flt_bilangan = float(bilangan)
+print()
+print("Tipe data bilangan     :", type(bilangan))
+print("Tipe data int_bilangan :", type(int_bilangan))
+print("Tipe data flt_bilangan :", type(flt_bilangan))
+print()
